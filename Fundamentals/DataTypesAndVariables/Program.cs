bd82d8d84b6b756a76c9d845056015c46d9b043d
Dara Oladapo <daraoladapo@outlook.com>
2021-01-07 16:13:02 +0000

    Data Types and Variables Module

---
+++ Fundamentals/DataTypesAndVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataTypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  myInteger = 5;
+            Console.WriteLine(myInteger);
+            double myDouble = 5.25;
+             Console.WriteLine(myDouble);
+            string myString = "Today is a beautiful day";
+             Console.WriteLine(myString);
+            char myChar = 'c';
+             Console.WriteLine(myChar);
+            bool myBool = true;
+             Console.WriteLine(myBool);
+
+            var myVar="Dara";
+             Console.WriteLine(myVar);
+            var myOtherVar=52;
+             Console.WriteLine(myOtherVar);
+        }
+    }
+}
