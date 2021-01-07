afdaf9a6053f29b96cfd846dd355a2d09212a973
Dara Oladapo <daraoladapo@outlook.com>
2021-01-07 16:47:19 +0000

    Making Decisions

---
+++ Fundamentals/MakingDecisions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MakingDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int age=18;
+           string Message=(age<18)?"Too young, please go home":"Welcome to the application";
+        //    int anotherAge=21;
+        //    int anotherAgeAdded=25;
+        //    if(age<20 || age < 25)
+        //    {
+        //        if(age<=18)
+        //         {
+        //             Console.WriteLine("Way too young to register.");
+        //         }
+        //        Console.WriteLine("Show your ID please.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Welcome to the application");
+        //    }
+
+        }
+    }
+}
