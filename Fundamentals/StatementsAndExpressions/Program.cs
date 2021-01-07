c0c8d08d1029d56cdae9096df18c7b5895c5f3a6
Dara Oladapo <daraoladapo@outlook.com>
2021-01-07 16:57:57 +0000

    statements and expressions

---
+++ Fundamentals/StatementsAndExpressions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StatementsAndExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string myMessage="Today is a beautiful day";
+            int age=5;
+            if(age<13)
+            {
+                string ageMessage="You are too young";
+                Console.WriteLine(ageMessage);
+            }
+        }
+    }
+}
