18012354cc82c439d080e3ad5e609f93a8a69ca8
Dara Oladapo <daraoladapo@outlook.com>
2021-01-30 19:39:16 +0000

    iterations

---
+++ Fundamentals/Iterations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for (int i = 3; i <= 100; i+=3)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // int[] myNumbers={1,2,4,5,66};
+            // foreach (var number in myNumbers)
+            // {
+            //     Console.WriteLine(number);
+            // }
+            int counter=5;
+            do
+            {
+                Console.WriteLine("{will run at least once} - Today is a beautiful day");
+                counter++;
+            } while (counter<=5);
+            while (counter<=5)
+            {
+                Console.WriteLine("{may new run} - Today is a beautiful day");
+                counter++;
+            }
+        }
+    }
+}
