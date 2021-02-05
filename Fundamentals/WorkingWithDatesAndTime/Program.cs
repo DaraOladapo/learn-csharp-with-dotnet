60d6b0d2992855d2b88f48f93b49ebd19cb75a78
Dara Oladapo <daraoladapo@outlook.com>
2021-02-05 08:44:55 +0000

    wip

---
+++ Fundamentals/WorkingWithDatesAndTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WorkingWithDatesAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDateAndTime=DateTime.Now;
+            Console.WriteLine(currentDateAndTime);
+            var currentShortTime=DateTime.Now.ToShortTimeString();
+            Console.WriteLine(currentShortTime);
+            var currentLongTime=DateTime.Now.ToLongTimeString();
+            Console.WriteLine(currentLongTime);
+            var currentShortDate=DateTime.Now.ToShortDateString();
+            
+            Console.WriteLine(currentShortDate);
+            var currentLongDate=DateTime.Now.ToLongDateString();
+            Console.WriteLine(currentLongDate);
+
+            var fifteenYearsAgo=DateTime.Now.AddYears(-15);
+            Console.WriteLine(fifteenYearsAgo.DayOfWeek);
+
+            var fifteenYearsToCome=DateTime.Now.AddYears(15);
+            Console.WriteLine(fifteenYearsToCome.DayOfWeek);
+        }
+    }
+}
