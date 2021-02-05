a02e6f4274eaa1459cfa7bdfcad235b6e9a1946a
Dara Oladapo <daraoladapo@outlook.com>
2021-02-05 17:09:26 +0000

    Classes and Inheritance

---
+++ Fundamentals/Classes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Car myCar = new Car();
+            // myCar.Make = "Toyota";
+            // myCar.Model = "Prius";
+            // myCar.Year = 2020;
+            // Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
+
+            // Car myOtherCar = new Car() { Make="Ford", Model="Focus",Year= 2019};
+            // Console.WriteLine($"{myOtherCar.Make} {myOtherCar.Model} {myOtherCar.Year}");
+
+            Car myOtherNiceCar = new Car("Nissan", "Juke", 2020);
+            myOtherNiceCar.Drive();
+            Utility.PrintDetails(myOtherNiceCar);
+        }
+    }
+    class Car{
+        public Car(){
+            Console.WriteLine("Creating a car");
+        }
+        public Car(string make, string model, int year){
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            Console.WriteLine($"{this.Make} {this.Model} {this.Year} has been created.");
+        }
+        public string Make;
+        public string Model;
+        public int Year;
+
+        public void Drive(){
+             Console.WriteLine($"{this.Make} {this.Model} {this.Year} is moving.");
+        }
+    }
+
+    static class Utility{
+        public static void PrintDetails(Car car){
+            Console.WriteLine($"{car.Make} was access {DateTime.Now.ToShortDateString()}");
+        }
+    }
+}
---
+++ Fundamentals/Inheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myTesla = new ElectricCar() { 
+                Make="Tesla",
+                Model="Model S",
+                Year=2021,
+                BatteryCapacity=50000
+            };
+            Console.Write($"{myTesla.Make} {myTesla.Model} has a battery capacity of {myTesla.BatteryCapacity}kwh");
+        }
+    }
+    }
+    class Car{
+        public Car(){
+            Console.WriteLine("Creating a car");
+        }
+        public Car(string make, string model, int year){
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            Console.WriteLine($"{this.Make} {this.Model} {this.Year} has been created.");
+        }
+        public string Make;
+        public string Model;
+        public int Year;
+
+        public void Drive(){
+             Console.WriteLine($"{this.Make} {this.Model} {this.Year} is moving.");
+        }
+    }
+class ElectricCar:Car{
+        public double BatteryCapacity;
+        public void Charge() {
+            Console.WriteLine("Charge increased");
+        }
+    }
+static class Utility
+{
+    public static void PrintDetails(Car car)
+    {
+        Console.WriteLine($"{car.Make} was access {DateTime.Now.ToShortDateString()}");
+    }
+}
