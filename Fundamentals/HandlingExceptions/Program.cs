da78a5853b07274f28fb5bff2138b4aaabf496d8
Dara Oladapo <daraoladapo@outlook.com>
2021-02-06 12:26:05 +0000

    currency convert

--- Fundamentals/HandlingExceptions/Program.cs
+++ Fundamentals/HandlingExceptions/Program.cs
@@ -6,7 +6,23 @@ namespace HandlingExceptions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int userAge;
+            Console.WriteLine("What year were you born?");
+            string userInput = Console.ReadLine();
+            try
+            {
+                int userYearOfBirth = int.Parse(userInput);
+                userAge = DateTime.Now.Year - userYearOfBirth;
+                Console.WriteLine($"Your age is {userAge}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Sorry, your input was not right.\n{e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
         }
     }
 }
---
+++ Fundamentals/SimpleCurrencyConverter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleCurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Dara's GBP to USD Converter.\nWhat currency do you have? 1 for GBP, 2 for USD");
+            var currencyFrom = (Currency)int.Parse(Console.ReadLine());
+            Console.WriteLine("How much do you want to convert?");
+            var amountToConvert = double.Parse(Console.ReadLine());
+            var convertedAmount = CurrencyUtility.Convert(amountToConvert, currencyFrom);
+            Console.WriteLine(convertedAmount);
+        }
+    }
+    static class CurrencyUtility{
+        public static double Convert(double amountValue, Currency currency){
+            double returnAmount=0;
+            if(currency==Currency.GBP){
+               returnAmount= amountValue * 1.37;
+            }
+            if(currency==Currency.USD){
+              returnAmount =amountValue / 1.37;
+            }
+            return returnAmount;
+        }
+    }    
+    enum Currency{
+        GBP=1,
+        USD
+    }
+}
